cc44bfdd1c9c3b475a4491075395e1cf9649e88b
Ross Hadden <rosshadden@gmail.com>
2022-08-22 15:37:50 -0400

    Added basic file reading.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,35 @@
 const std = @import("std");
 
-pub fn main() anyerror!void {
-  std.log.info("All your codebase are belong to us.", .{});
+const expect = std.testing.expect;
+
+pub fn main() !void {
+  const args = std.os.argv[1..std.os.argv.len];
+
+  var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+  const allocator = gpa.allocator();
+
+  switch (args.len) {
+    0 => {
+      // runPrompt();
+    },
+    1 => {
+      const path = std.mem.sliceTo(args[0], '0');
+      try runFile(allocator, path);
+    },
+    else => {
+      std.log.err("Usage: lox [script]", .{});
+      std.process.exit(0);
+    },
+  }
 }
 
-test "basic test" {
-  try std.testing.expectEqual(10, 3 + 7);
+fn runFile(allocator: std.mem.Allocator, path: []const u8) !void {
+  const source = try std.fs.cwd().readFileAlloc(allocator, path, 1_000_000);
+  defer allocator.free(source);
+
+  run(source);
+}
+
+fn run(source: []u8) void {
+  std.debug.print("{s}", .{source});
 }
