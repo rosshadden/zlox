f091fe4fa9fe5edce6c5de67479d893ce90ed2ee
Ross Hadden <rosshadden@gmail.com>
2022-09-19 00:29:11 -0400

    Added parser skeleton.
    Signed-off-by: Ross Hadden <rosshadden@gmail.com>

--- src/lib/parser.zig
+++ src/lib/parser.zig
@@ -1,29 +1,87 @@
 const std = @import("std");
 
-const Token = @import("./tokens.zig").Token;
+const expressions = @import("./expressions.zig");
+const tokens = @import("./tokens.zig");
 
 pub const Parser = struct {
   const Self = @This();
 
   allocator: std.mem.Allocator,
-  tokens: std.ArrayList(Token),
+  tokens: std.ArrayList(tokens.Token),
   current: usize = 0,
 
-  pub fn init(alc: std.mem.Allocator, tokens: std.ArrayList(Token)) Self {
+  pub fn init(alc: std.mem.Allocator, tkns: std.ArrayList(tokens.Token)) Self {
     return Self{
       .allocator = alc,
-      .tokens = tokens,
+      .tokens = tkns,
     };
   }
 
-  pub fn deinit(_: *Self) void {
+  pub fn deinit(_: *Self) void {}
+
+  fn expression(self: *Self) *expressions.Expr {
+    return self.equality();
+  }
+
+  fn equality(self: *Self) *expressions.Expr {
+    const expr = self.comparison();
+    return expr;
+  }
+
+  fn comparison(self: *Self) *expressions.Expr {
+    const expr = self.term();
+    return expr;
+  }
+
+  fn term(self: *Self) *expressions.Expr {
+    const expr = self.factor();
+    return expr;
+  }
+
+  fn factor(self: *Self) *expressions.Expr {
+    const expr = self.unary();
+    return expr;
+  }
+
+  fn unary(self: *Self) *expressions.Expr {
+    const expr = self.primary();
+    return expr;
+  }
+
+  fn primary(_: *Self) *expressions.Expr {
+    return expressions.Expr{
+      .literal = .{
+        .value = tokens.Literal.nil,
+      },
+    };
+  }
+
+  fn match(_: *Self) bool {
+    return false;
+  }
+
+  fn check(_: *Self) bool {
+    return false;
+  }
+
+  fn advance(_: *Self) void {
+  }
+
+  fn isAtEnd(_: *Self) bool {
+    return true;
+  }
+
+  fn peek(_: *Self) void {
+  }
+
+  fn previous(_: *Self) void {
   }
 };
 
 test "init" {
-  var tokens = std.ArrayList(Token).init(std.testing.allocator);
-  defer tokens.deinit();
-  var parser = Parser.init(std.testing.allocator, tokens);
+  var tkns = std.ArrayList(tokens.Token).init(std.testing.allocator);
+  defer tkns.deinit();
+  var parser = Parser.init(std.testing.allocator, tkns);
   defer parser.deinit();
   std.debug.print("{}\n", .{ parser.current });
 }
